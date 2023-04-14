3b6458403d311a1066fbd93bf0fe5ac9559510e3
Daniel Tello <dantello.319@gmail.com>
2023-04-14 17:56:28 -0500

    funcionalidad timer y botones

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Color = UnityEngine.Color;
 
 public class Pregunta
 {
@@ -27,12 +29,14 @@ public class GameManager : MonoBehaviour
    public Button butOpcion4;
 
    private List<Pregunta> listaPreguntas;
-   private int index = 0;
+   private int index = 1;
+
+   private float timer = 20f;
 
    private void Start()
    {
       CrearPreguntas();
-      CargarPregunta(0);
+      CargarPregunta(index);
    }
 
    private void CrearPreguntas()
@@ -73,9 +77,43 @@ public class GameManager : MonoBehaviour
       textOpt4.text = pregunta.Opciones[3];
    }
 
+   public void SeleccionarOpcion(int num)
+   {
+      Pregunta pregunta = listaPreguntas[index];
+      Color color = pregunta.IndexCorrecta == num ? Color.green : Color.red;
+
+      var colors = butOpcion1.colors;
+      colors.normalColor = color;
+      colors.highlightedColor = color;
+      colors.selectedColor = color;
+
+      switch (num)
+      {
+         case 0:
+            butOpcion1.colors = colors;
+            break;
+         case 1:
+            butOpcion2.colors = colors;
+            break;
+         case 2:
+            butOpcion3.colors = colors;
+            break;
+         case 3:
+            butOpcion4.colors = colors;
+            break;
+      }
+   }
+
 
    private void Update()
    {
-
+      timer -= Time.deltaTime;
+      TextoTimer.text = Mathf.RoundToInt(timer).ToString();
+      if (timer < 0f)
+      {
+         //acabo el tiempo
+         TextoTimer.text = "-";
+      }
+      
    }
 }
